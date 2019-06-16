f7d955f2670396e65dfaca1447d34166e67aa684
League Student <league@iMac-47.attlocal.net>
2019-06-16 11:24:58 -0700

    more code
    :octocat:

---
+++ src/are_you_happy.java
@@ -0,0 +1,117 @@
+import javax.swing.JOptionPane;
+
+public class are_you_happy {
+
+public static void main(String[] args) {
+
+	String hi = JOptionPane.showInputDialog("Are You Happy?");
+
+if(hi.equals("yes")) {
+	JOptionPane.showMessageDialog(null, "keep doing whatever your doing");
+}
+
+
+else if(hi.equals("no")) { 
+	
+	JOptionPane.showInputDialog("do you want to be happy?");
+	if(hi.contentEquals("no")) {
+		
+		
+	}
+	
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}}
