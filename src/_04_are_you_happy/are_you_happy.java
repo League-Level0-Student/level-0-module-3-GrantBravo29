262bac7958ee78cdc9e264dbf0cccc951beefea7
League Student <league@wtsim17.attlocal.net>
2019-06-23 11:24:32 -0700

    hi

--- src/_04_are_you_happy/are_you_happy.java
+++ src/_04_are_you_happy/are_you_happy.java
@@ -1,8 +1,10 @@
+package _04_are_you_happy;
 import javax.swing.JOptionPane;
 
 public class are_you_happy {
 
 public static void main(String[] args) {
+	
 
 	String hi = JOptionPane.showInputDialog("Are You Happy?");
 
@@ -13,105 +15,18 @@ if(hi.equals("yes")) {
 
 else if(hi.equals("no")) { 
 	
-	JOptionPane.showInputDialog("do you want to be happy?");
-	if(hi.contentEquals("no")) {
-		
-		
+	String hii= JOptionPane.showInputDialog("do you want to be happy?");
+	if(hii.equals("no")) {
+		JOptionPane.showMessageDialog(null,"keep doing whatever your doing" );
+	}
+	else if(hii.equals("yes"))	{
+		JOptionPane.showMessageDialog(null, "change something");	
+			
+	}
+		}
 	}
 	
 }
-}
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}}
