5232789def44e7e44f1a47cbe5b5acb6cde25c79
League Student <league@iMac-26.attlocal.net>
2019-06-30 11:26:26 -0700

    horoscope
    I accidently did all the horoscopes :neutral_face:

---
+++ src/_05_horoscope/horoscope.java
@@ -0,0 +1,150 @@
+package _05_horoscope;
+
+import javax.swing.JOptionPane;
+
+public class horoscope {
+public static void main(String[] args) {
+	
+
+
+
+
+String hi = JOptionPane.showInputDialog("what is your star sign");
+
+
+
+if(hi.equals("aries")){
+	
+	
+	JOptionPane.showMessageDialog(null, "Be careful with those around you today and question if you can trust them, pay attention to warnings.\n" + 
+			"\n" + 
+			"Pay particular attention to the company you are keeping today and be careful about what you say to who.\n Are you sure you can trust the people around you? Are you fully aware of their agendas and intentions? You dont need to be paranoid, but if you see warning signs and red flags take them seriously, and listen if those you do trust give you advice.");
+}
+
+if(hi.equals("taurus")) {
+	
+	
+	JOptionPane.showMessageDialog(null, "Question your friends and their motives today and youll find out who your real friends are.\n" + 
+			"\n" + 
+			"With some suspicious energy around today, ask questions of those around you and when friends want you to do things with them, question their motives."
+			+ " You might find out who your real friends are today, but thats no bad thing. \n You only need those in your life who truly love and support you.\n Negative and false people need to go.");
+	
+	
+	
+}
+
+if(hi.equals("gemini")){
+	
+JOptionPane.showMessageDialog(null, "As The Moon enters your house today use its energy to observe someones true colours.\n" + 
+		"\n" + 
+		"The Moon enters Gemini today and this is creating strong energy around you. This will help you to see more clearly where others around you are concerned.\n Listen to what they say and observe them and youll see their true colours emerging. This may be disappointing in some cases, but its better to know now if someone is not what you thought.");
+	
+	
+}
+
+if(hi.equals("cancer")) {
+	
+	
+	
+	JOptionPane.showMessageDialog(null, "As a new relationship journey begins in your life today use your communications skills and be honest.\n" + 
+			"\n" + 
+			"A new relationship journey is about to begin in your life and you have strong communication to make sure this gets off to a good start.\n This could be a new adventure for you and someone already in your life or it could be a new relationship altogether, but either way be honest and authentic in how you go forward with this person.");
+}
+
+
+if(hi.equals("leo")) {
+	
+	
+	JOptionPane.showMessageDialog(null, "Youll need to be direct and clear with your communication today, speak plainly so others understand.\n" + 
+			"\n" + 
+			"Your energy is a bit out of sync today and this will cause particular problems in communication for you. Make sure you are being very clear when you talk to others and dont leave any room for misunderstandings.\n If you want someone to know something youll have to tell them directly, dont think hints will be enough for them to understand.");
+}
+
+if(hi.equals("virgo")){
+	
+	JOptionPane.showMessageDialog(null, "Its not the day for being competitive today and when making new connections dont label them too early.\n" + 
+			"\n" + 
+			"Its a good day for making connections today but when it comes to new conversations with new people be careful you dont put them in pigeon holes or label them straight off, give these things time to emerge and evolve.\n Stay away from competition, you might have a strong competitive streak but today is not the day for this.");
+}
+
+
+
+if(hi.equals("libra")) {
+	JOptionPane.showMessageDialog(null, "As family issues come to the surface today, deal with them and be honest about how you feel.\n" + 
+			"\n" + 
+			"Unresolved issues in your family life will come to the surface today and its the right time to face these and deal with them. This could be difficult and uncomfortable, especially where you have strong differences of opinion.\n You might feel there are too many demands on you and youre finding it hard to balance, communicate this clearly.");
+	
+}
+
+if(hi.equals("scorpio")) {
+	JOptionPane.showMessageDialog(null, "As your day is calm and settled today you can consider decisions you have to make and your journey ahead.\n" + 
+			"\n" + 
+			"Things in your life will settle today and a calmness descends on your day. You can use this time to consider in detail any decisions you have to make and to contemplate what you want and where you want to go.\n You have a very clear head and you understand situations much better. The only area to be wary of is in public speaking as you might not articulate yourself well.");
+	
+}
+
+if(hi.equals("sagittarius")) {
+	JOptionPane.showMessageDialog(null, "You need to adapt to a slower pace today, so give yourself time and space to consider your situation.\n" + 
+			"\n" + 
+			"As energy calms in your house today and all around you, you might find it difficult at first to adapt from such a fast paced environment to one of quiet and peace. Take your time and give yourself the space you need to adjust./n This is a great day for finding your feet in your own life again and for contemplating your future. \n"
+			+ "You can make solid decisions now.");
+	
+}
+if(hi.equals("capricorn")) {
+JOptionPane.showMessageDialog(null, "If you find it hard to make decisions today, dont make any, theres nothing wrong with waiting and watching.\n" + 
+		"\n" + 
+		"You may find it hard to concentrate today as your energy is quite scattered. This could lead you to standing still and agreeing with everyone around you, simply because you cant make decisions.\n Remember, it is better to not move at all than to move without a plan or an idea of where you are going.\n Take your time and dont feel pushed into anything.");	
+	
+}
+
+
+
+if(hi.equals("aquarius")) {
+	JOptionPane.showMessageDialog(null, "You are in demand from lots of different areas today due to your fingers being in lots of pies, take your time.\n" + 
+			"\n" + 
+			"As all the irons you have in the fire start to heat up at once today youll find you are in demand from lots of different areas. This can be overwhelming so take some time to think about what your next move is.\n Dont expect sympathy and understanding from others, they may be jealous of your position.\n You dont need them, this is your moment.");
+	
+	
+}
+
+if(hi.equals("pisces")) {
+	JOptionPane.showMessageDialog(null, "Use your social energy today to make new connections and to re-connect with old friends.\n" + 
+			"\n" + 
+			"You have great social energy today and its a good time to meet new people and make new friends and connections.\n You can also gain new knowledge through other people today especially in your work life where your closer relationships with colleagues will help move you forward.\n Its a good day to re-connect with old friends too.");
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
