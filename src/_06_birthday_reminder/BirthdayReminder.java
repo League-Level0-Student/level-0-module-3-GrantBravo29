d1d75de83c6ca23eea029ae3d496eb01be29ce04
League Student <league@iMac-17.attlocal.net>
2019-07-14 11:27:32 -0700

    stuff
    hi :scissors:

--- src/_06_birthday_reminder/BirthdayReminder.java
+++ src/_06_birthday_reminder/BirthdayReminder.java
@@ -11,21 +11,47 @@ public class BirthdayReminder {
 	public static void main(String[] args) {
 	
 		// 1. correct the birthdays for your family below
-		String momsBirthday = "January 24th";
-		String dadsBirthday = "June 9th";
-		String myBirthday = "April 16th";
+		String dogsBirthday = "August 21th";
+		String dadsBirthday = "May 13th";
+		String myBirthday = "September 22nd";
 
 		// 2. Find out which birthday the user wants and and store their response in a variable
-	
+String hi = JOptionPane.showInputDialog("which birthday do you want?");
 		// 3. Print out what the user typed
-		
-		// 4. if user asked for "mom"
-			//print mom's birthday
-		// 5. if user asked for "dad"
-			// print dad's birthday
-		// 6. if user asked for your name
-			// print myBirthday
-		//7. otherwise print "Sorry, i don't remember that person's birthday!"
+	
 
+if(hi.equals(myBirthday)) {
+	
+	JOptionPane.showMessageDialog(null, "September 22nd");
+	
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	} 
 }
